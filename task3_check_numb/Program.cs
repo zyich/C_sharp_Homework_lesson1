1d27df7b7674e2f2235fc9362af30a305be2701d
zyich <zuilinho@yandex.ru>
2023-06-15 23:32:26 +0200

    task 3 check numb / 2 or no

---
+++ task3_check_numb/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 6: Напишите программу, которая 
+на вход принимает число и выдаёт, является
+ ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Input numberA =>");
+int numberA  = Convert.ToInt32(Console.ReadLine());
+float remainder = numberA % 2;
+if (remainder ==  0)
+{
+    Console.WriteLine("число четное");
+ }
+ else
+ {
+    Console.WriteLine("число нечетное");
+ }
+
+
+
+
+
+
+
+
+
+
+
