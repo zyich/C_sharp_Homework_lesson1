c045b0193d90a6ade8ffb5068bc7fd5f93105b7c
zyich <zuilinho@yandex.ru>
2023-06-15 20:03:15 +0200

    task 2 find bigger in 3 number

---
+++ task2_find_bigger_in_3numb/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 4: Напишите программу, которая 
+принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Input numberA =>");
+int numberA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input numberB =>");
+int numberB  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input numberC =>");
+int numberC  = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > numberA)
+{
+    max = numberB;
+}
+if  (numberC > max)
+{ 
+    max = numberC;
+}
+Console.WriteLine($"Max number: {max}");
