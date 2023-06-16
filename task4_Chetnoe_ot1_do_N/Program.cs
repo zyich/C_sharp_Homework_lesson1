9dbe775116e39f8b7e8dc56304265951af9c4416
zyich <zuilinho@yandex.ru>
2023-06-16 10:52:22 +0200

    task4 all numbers/2 between 1 and N

---
+++ task4_Chetnoe_ot1_do_N/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 8: Напишите программу, которая 
+на вход принимает число (N), а на выходе
+ показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+/*Console.Write("Input numberA =>");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= N ; i += 2 )
+{
+    Console.WriteLine(i);
+}
+*/
+
+
+
+
+Console.Write("Введите число N => ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N >= 0)
+{
+    for (int i = 0; i <= N; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    for (int i = 0; i >= N; i -= 2)
+    {
+        Console.WriteLine(i);
+    }
+}
